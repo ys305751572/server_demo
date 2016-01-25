e94705880563967b181d285f8afb019fbe42433a
ys305751572 <305751572@qq.com>
2016-01-25 19:19:42 +0800

    1

--- src/main/java/com/leoman/module/Module.java
+++ src/main/java/com/leoman/module/Module.java
@@ -1,5 +1,6 @@
 package com.leoman.module;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -11,4 +12,36 @@ public class Module extends BaseEntity{
 
 	private static final long serialVersionUID = 986592508272421306L;
 
+	private String moduleName;
+	
+	private String url;
+	
+	private String desc;
+
+	@Column(name="module_name")
+	public String getModuleName() {
+		return moduleName;
+	}
+
+	public void setModuleName(String moduleName) {
+		this.moduleName = moduleName;
+	}
+
+	@Column(name="url")
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	@Column(name="desc")
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
 }
