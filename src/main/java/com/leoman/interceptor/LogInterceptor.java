75fd91515b37f0a2d30b7e941bd2a54d3480c756
ys305751572 <305751572@qq.com>
2016-01-25 14:35:16 +0800

    1

---
+++ src/main/java/com/leoman/interceptor/LogInterceptor.java
@@ -0,0 +1,8 @@
+package com.leoman.interceptor;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class LogInterceptor extends HandlerInterceptorAdapter{
+
+	
+}
