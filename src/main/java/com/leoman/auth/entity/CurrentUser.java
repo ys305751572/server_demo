07255d46813237dcb2120e43a2a32dd80d286ca2
ys305751572 <305751572@qq.com>
2016-02-02 14:50:17 +0800

    1

--- src/main/java/com/leoman/auth/entity/CurrentUser.java
+++ src/main/java/com/leoman/auth/entity/CurrentUser.java
@@ -5,7 +5,23 @@ import org.apache.shiro.subject.Subject;
 
 public final class CurrentUser {
 
-	protected Subject getSubject() {
+	/**
+	 * 获取当前用户
+	 * @return
+	 */
+	public static Principal getCurrentUser() {
+		return (Principal) getSubject().getPrincipal() ;
+	}
+	
+	public static String getAccount() {
+		return getCurrentUser().getUsername();
+	}
+	
+	public static Long getId() {
+		return getCurrentUser().getId();
+	}
+	
+	protected static Subject getSubject() {
 		return SecurityUtils.getSubject();
 	}
 }
---
+++ src/main/java/com/leoman/utils/InvokeResult.java
@@ -0,0 +1,46 @@
+package com.leoman.utils;
+
+public class InvokeResult {
+
+	private Object data;
+
+	private String errorMessage;
+
+	private boolean hasErrors;
+
+	public static InvokeResult success(Object data) {
+		InvokeResult result = new InvokeResult();
+		result.data = data;
+		result.hasErrors = false;
+		return result;
+	}
+
+	public static InvokeResult success() {
+		InvokeResult result = new InvokeResult();
+		result.hasErrors = false;
+		return result;
+	}
+
+	public static InvokeResult failure(String message) {
+		InvokeResult result = new InvokeResult();
+		result.hasErrors = true;
+		result.errorMessage = message;
+		return result;
+	}
+
+	public Object getData() {
+		return data;
+	}
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+	public boolean isHasErrors() {
+		return hasErrors;
+	}
+
+	public boolean isSuccess() {
+		return !hasErrors;
+	}
+}
