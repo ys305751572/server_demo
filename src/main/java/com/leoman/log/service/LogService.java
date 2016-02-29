ec3203372f989fea815e99a2af51b5959d96e501
ys305751572 <305751572@qq.com>
2016-02-29 17:19:26 +0800

    1

--- src/main/java/com/leoman/log/service/LogService.java
+++
@@ -1,8 +0,0 @@
-package com.leoman.log.service;
-
-import com.leoman.baisc.service.BaseService;
-import com.leoman.log.entity.Log;
-
-public interface LogService extends BaseService<Log, Long>{
-
-}
